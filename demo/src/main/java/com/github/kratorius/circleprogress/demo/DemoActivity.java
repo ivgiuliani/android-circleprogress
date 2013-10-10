33f26a056db6c62fe7fff11645f75b3c5b78d547
Ivan Giuliani <giuliani.v@gmail.com>
2013-10-10 13:34:11 +0200

    Demo activity draft

--- demo/src/main/java/com/github/kratorius/circleprogress/demo/DemoActivity.java
+++ demo/src/main/java/com/github/kratorius/circleprogress/demo/DemoActivity.java
@@ -1,12 +1,46 @@
 package com.github.kratorius.circleprogress.demo;
 
 import android.app.Activity;
+import android.graphics.drawable.ScaleDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.kratorius.circleprogress.CircleProgressView;
 
 public class DemoActivity extends Activity {
+    private Button btnFadeInReload;
+    private Button btnRollReload;
+    private Button btnRollIncremental;
+
+    private CircleProgressView crcBackgroundLine;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.demo_layout);
+
+        btnFadeInReload = (Button) findViewById(R.id.btn_reload_fade_in);
+        btnRollReload = (Button) findViewById(R.id.btn_reload_roll);
+        btnRollIncremental = (Button) findViewById(R.id.btn_reload_incremental);
+        crcBackgroundLine = (CircleProgressView) findViewById(R.id.crc_bg2);
+
+        Reloader reloader = new Reloader();
+
+        btnFadeInReload.setOnClickListener(reloader);
+        btnRollReload.setOnClickListener(reloader);
+        btnRollIncremental.setOnClickListener(reloader);
+
+        /*ScaleDrawable scaleDraw = (ScaleDrawable) getResources().getDrawable(R.drawable.circle3_bg);
+        scaleDraw
+        crcBackgroundLine.setB*/
+    }
+
+    private class Reloader implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            finish();
+            startActivity(getIntent());
+        }
     }
 }
